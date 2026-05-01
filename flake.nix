4b5052309bd0b963c7cae6faf24f975746dd3170
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-05-01 15:37:59 +0100

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Rio Tinto Tennis court booking app";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+            uv
+          ];
+
+          shellHook = ''
+            echo "Rio Tinto dev shell"
+            echo "Node $(node --version) | npm $(npm --version)"
+          '';
+        };
+      });
+}
